66f0b0cb25aff454821f85996f16623d65228cd3
Matheus Vieira <matheustsr@outlook.com>
2021-06-02 07:16:17 -0300

    fix routes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require("./routes")
 
-const server = express()
-const videos = require("./data")
+const server = express() // Create a server
 
-server.use(express.static('public'))
+/*
+Middlewares (interceptadores de requisição)
+*/
+
+server.use(express.static('public')) // Listening public folder
+server.use(routes)
 
 server.set("view engine", "njk")
 
@@ -14,41 +19,6 @@ nunjucks.configure("views", {
     noCache: true
 })
 
-server.get("/", function (req, res) {
-    const about = {
-        avatar_url: "https://avatars3.githubusercontent.com/u/6643122?s=460&v=4",
-        name: "Mayk Brito",
-        role: "Instrutor - Rocketseat",
-        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
-        links: [
-            { name: "Github", url: "https://github.com/maykbrito/" },
-            { name: "Twitter", url: "https://twitter.com/maykbrito/" },
-            { name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/" }
-        ]
-    }
-
-
-    return res.render("about", { about })
-})
-
-server.get("/portfolio", function (req, res) {
-    return res.render("portfolio", { items: videos })
-})
-
-server.get("/video", function(req, res) {
-    const id = req.query.id
-
-    const video = videos.find(function(video){
-        return video.id == id
-    })
-
-    if (!video) {
-        return res.send("Video not found!")
-    }
-
-    return res.render("video", { item: video })
-})
-
-server.listen(5000, function () {
-    console.log(`server is running...`) 
+server.listen(5000, function () { // make on
+    console.log('server is running[✔]') 
 })
